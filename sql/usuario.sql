e5dee46aa9e76d943d7d41ac930d911f2543769d
Fábio Valentim <maitabom@gmail.com>
2021-02-02 21:35:23 -0300

    Adição de script de usuário administrador

---
+++ sql/usuario.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO `usuario` (`id`, `usuario`, `nome`, `email`, `senha`, `grupo`, `ativo`, `suspenso`, `verificar`, `prefid`) VALUES
+(1, 'admin', 'Administrador do Sistema', 'admin@rh.com.br', '7c4a8d09ca3762af61e59520943dc26494f8941b', 1, 1, 0, 0, NULL);
