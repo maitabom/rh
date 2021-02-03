4c88466f5cd0984dafc45c47388a3d097dfdd3e1
Fábio Valentim <maitabom@gmail.com>
2021-02-02 21:34:37 -0300

    Adição de script de funções

--- sql/funcoes.sql
+++ sql/funcoes.sql
@@ -1,51 +1,53 @@
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Usuários', 'listar_usuarios', '1', '1');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Novo Usuário', 'adicionar_usuario', '1', '2');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar Usuário', 'editar_usuario', '1', '3');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Usuário', 'excluir_usuario', '1', '4');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Grupos', 'listar_grupos_usuario', '2', '5');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Novo Grupo', 'adicionar_grupo_usuario', '2', '6');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar Grupo', 'editar_grupo_usuario', '2', '7');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Grupo', 'excluir_grupo_usuario', '2', '8');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar IPs', 'listar_ips_firewall', '3', '9');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Adicionar IPs', 'adicionar_ips_firewall', '3', '10');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar IPs', 'editar_ips_firewall', '3', '11');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir IPs', 'excluir_ips_firewall', '3', '12');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Secretarias', 'listar_secretarias', '4', '13');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Nova Secretaria', 'adicionar_secretaria', '4', '14');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar Secretaria', 'editar_secretaria', '4', '15');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Secretaria', 'excluir_secretaria', '4', '16');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Banners', 'listar_banners', '5', '17');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Novo Banner', 'adicionar_banner', '5', '18');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar Banner', 'editar_banner', '5', '18');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Banner', 'excluir_banner', '5', '18');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Publicações', 'listar_publicacoes', '6', '19');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Nova Publicação', 'adicionar_publicacao', '6', '20');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar Publicação', 'editar_publicacao', '6', '21');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Publicação', 'excluir_publicacao', '6', '22');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Licitações', 'listar_licitacoes', '7', '23');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Nova Licitação', 'adicionar_licitacao', '7', '24');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar Licitação', 'editar_licitacao', '7', '25');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Licitação', 'excluir_licitacao', '7', '26');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Notícias', 'listar_noticias', '8', '27');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Nova Notícia', 'adicionar_noticia', '8', '28');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar Notícia', 'editar_noticia', '8', '29');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Notícia', 'excluir_noticia', '8', '30');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Legislação', 'listar_legislacao', '9', '31');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Adicionar Legislação', 'adicionar_legislacao', '9', '32');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar Legislação', 'editar_legislacao', '9', '33');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Legislação', 'excluir_legislacao', '9', '34');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Categorias', 'listar_categorias_legislacao', '10', '35');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Nova Categoria', 'adicionar_categoria_legislacao', '10', '36');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar Categoria', 'editar_categoria_legislacao', '10', '37');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Categoria', 'excluir_categoria_legislacao', '10', '38');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Páginas', 'listar_paginas', '11', '39');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Adicionar Página', 'adicionar_pagina', '11', '40');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar Página', 'editar_pagina', '11', '41');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Página', 'excluir_pagina', '11', '42');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Controles', 'listar_controles_home', '12', '43');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Adicionar Controle', 'adicionar_controle_home', '12', '44');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Editar Controle', 'editar_controle_home', '12', '45');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Controle', 'excluir_controle', '12', '46');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Listar Auditoria', 'listar_auditoria', '13', '47');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Detalhes do Registro', 'detalhes_registro_auditoria', '13', '48');
-INSERT INTO `coqueiralmg`.`funcoes` (`nome`, `chave`, `grupo`, `ordem`) VALUES ('Excluir Registro', 'excluir_registro_auditoria', '13', '49');
+
+INSERT INTO `funcao` (`id`, `nome`, `chave`, `grupo`, `ordem`, `ativo`) VALUES
+(1, 'Listar Usuários', 'listar_usuarios', 1, 1, 1),
+(2, 'Adicionar Usuário', 'adicionar_usuarios', 1, 2, 1),
+(3, 'Editar Usuário', 'editar_usuarios', 1, 3, 1),
+(4, 'Excluir Usuário', 'excluir_usuarios', 1, 4, 1),
+(5, 'Listar Grupos', 'listar_grupos_usuarios', 2, 5, 1),
+(6, 'Adicionar Grupo', 'adicionar_grupos_usuarios', 2, 6, 1),
+(7, 'Editar Grupo', 'editar_grupo_usuario', 2, 7, 1),
+(8, 'Excluir Grupo', 'excluir_grupo_usuario', 2, 8, 1),
+(9, 'Listar IPs', 'listar_ips_firewall', 3, 9, 1),
+(10, 'Adicionar IPs', 'adicionar_ips_firewall', 3, 10, 1),
+(11, 'Editar IPs', 'editar_ips_firewall', 3, 11, 1),
+(12, 'Excluir IPs', 'excluir_ips_firewall', 3, 12, 1),
+(13, 'Listar Funcionários', 'listar_funcionarios', 4, 13, 1),
+(14, 'Adicionar Funcionários', 'adicionar_funcionarios', 4, 14, 1),
+(15, 'Editar Funcionários', 'editar_funcionarios', 4, 15, 1),
+(16, 'Excluir Funcionários', 'excluir_funcionarios', 4, 16, 1),
+(17, 'Listar Atestados', 'listar_atestados', 5, 17, 1),
+(18, 'Adicionar Atestados', 'adicionar_atestados', 5, 18, 1),
+(19, 'Editar Atestados', 'editar_atestados', 5, 19, 1),
+(20, 'Excluir Atestados', 'excluir_atestados', 5, 20, 1),
+(21, 'Listar Médicos', 'listar_medicos', 6, 21, 1),
+(22, 'Adicionar Médicos', 'adicionar_medicos', 6, 22, 1),
+(23, 'Editar Médicos', 'editar_medicos', 6, 23, 1),
+(24, 'Excluir Médicos', 'excluir_medicos', 6, 24, 1),
+(25, 'Visualizar Funcionários', 'visualizar_funcionarios', 4, 15, 1),
+(26, 'Visualizar Atestados', 'visualizar_atestados', 5, 19, 1),
+(27, 'Imprimir Usuários', 'imprimir_usuarios', 1, 5, 1),
+(28, 'Imprimir Grupos', 'imprimir_grupos', 2, 13, 1),
+(29, 'Imprimir IPs', 'imprimir_ips_firewall', 3, 13, 1),
+(30, 'Imprimir Funcionários', 'imprimir_funcionarios', 4, 16, 1),
+(31, 'Imprimir Atestados', 'imprimir_atestados', 5, 20, 1),
+(32, 'Imprimir Médicos', 'imprimir_medicos', 6, 25, 1),
+(33, 'Visualizar Médicos', 'visualizar_medicos', 6, 24, 1),
+(34, 'Listar Auditoria', 'listar_auditoria', 7, 26, 1),
+(35, 'Detalhes do Registro', 'detalhes_registro_auditoria', 7, 27, 1),
+(36, 'Excluir Registro', 'excluir_registro_auditoria', 7, 28, 1),
+(38, 'Imprimir Auditoria', 'imprimir_auditoria', 7, 30, 1),
+(44, 'Listar CID', 'listar_cid', 8, 31, 1),
+(45, 'Imprimir CID', 'imprimir_cid', 8, 32, 1),
+(46, 'Visualizar CID', 'visualizar_cid', 8, 33, 1),
+(47, 'Adicionar CID', 'adicionar_cid', 8, 34, 1),
+(48, 'Editar CID', 'editar_cid', 8, 35, 1),
+(49, 'Excluir CID', 'excluir_cid', 8, 36, 1),
+(50, 'Visão Geral de Relatórios', 'relatorio_visao', 9, 37, 1),
+(51, 'Funcionario por Atestado', 'relatorio_funcionario_atestado', 9, 38, 1),
+(52, 'Empresas por Atestado', 'relatorio_empresas_atestado', 9, 39, 1),
+(53, 'Relatorio de CIDs', 'relatorio_cid', 9, 42, 1),
+(54, 'Cargos por Atestado', 'relatorio_cargo_atestado', 9, 40, 0),
+(55, 'Tipo Func. Atestado', 'relatorio_tipo_funcionario_atestado', 9, 41, 0),
+(56, 'Medicos Por Atestados', 'relatorios_medicos_atestados', 9, 43, 1);
+
