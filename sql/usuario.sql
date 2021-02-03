9da364e1b8b68af88eddb83e96b0093c17c306d0
Fábio Valentim <maitabom@gmail.com>
2021-02-02 22:18:07 -0300

    Ajustes do sistema do usuario

--- sql/usuario.sql
+++ sql/usuario.sql
@@ -1,3 +1,1 @@
-
-INSERT INTO `usuario` (`id`, `usuario`, `nome`, `email`, `senha`, `grupo`, `ativo`, `suspenso`, `verificar`, `prefid`) VALUES
-(1, 'admin', 'Administrador do Sistema', 'admin@rh.com.br', '7c4a8d09ca3762af61e59520943dc26494f8941b', 1, 1, 0, 0, NULL);
+select * from acao
