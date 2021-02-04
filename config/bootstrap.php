c4ec15172b4346108d2b2a6836a8574940748d0b
Fábio Valentim <maitabom@gmail.com>
2021-02-04 20:50:44 -0300

    Migração da configuração do email transport

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -60,6 +60,7 @@ use Cake\Datasource\ConnectionManager;
 use Cake\Error\ErrorHandler;
 use Cake\Log\Log;
 use Cake\Mailer\Email;
+use Cake\Mailer\TransportFactory;
 use Cake\Network\Request;
 use Cake\Utility\Inflector;
 use Cake\Utility\Security;
@@ -161,7 +162,7 @@ if (!Configure::read('App.fullBaseUrl')) {
 
 Cache::setConfig(Configure::consume('Cache'));
 ConnectionManager::setConfig(Configure::consume('Datasources'));
-Email::setConfigTransport(Configure::consume('EmailTransport'));
+TransportFactory::setConfig(Configure::consume('EmailTransport'));
 Email::setConfig(Configure::consume('Email'));
 Log::setConfig(Configure::consume('Log'));
 Security::salt(Configure::consume('Security.salt'));
